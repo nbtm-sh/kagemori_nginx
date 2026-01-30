63ff8a0f636bde763db8b2ec6fa91b903af806e5
nbtm-sh <dog@nbtm.sh>
2026-01-30 14:09:00 +1100

    fix(dir): remove debug statements from dir wrapper

--- kagemori_nginx/dir.py
+++ kagemori_nginx/dir.py
@@ -1,20 +1,15 @@
 import os
 def create_directory_for_path(path):
-    print(path)
     dirname = os.path.dirname(path)
-    print(dirname)
 
     if os.path.exists(dirname) and not os.path.exists(path):
-        print("Creating")
         os.makedirs(path)
         return
 
     if not dirname:
         dirname = path
-        print(dirname)
 
     if os.path.exists(dirname):
-        print("Exists - exiting")
         return
-    print("Creating")
+
     os.makedirs(dirname)
